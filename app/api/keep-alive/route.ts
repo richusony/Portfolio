1c99b24ccbbf9b7bc1fe640745ac0f7a91108289
Richu Sony <sonyrichu4@gmail.com>
2025-01-31 18:29:37 +0530

    update: api response message

--- app/api/keep-alive/route.ts
+++ app/api/keep-alive/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    return NextResponse.json("Portfolio server re-activatied...", { status: 200 });
+    return NextResponse.json({message: "Portfolio: server re-activated"});
 }
